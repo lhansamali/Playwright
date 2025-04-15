b8e18cf176b0b50ebbf18c08b7d2b2975d60b5d6
lhansamali <lhansamali@gmail.com>
2025-04-15 22:34:33 +0530

    Added Assertions

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -3,13 +3,15 @@ import { test, expect } from '@playwright/test';
 test.describe('Search Items',()=>{
     test.beforeEach('Login',async ({page})=>{
         await page.goto("https://practicesoftwaretesting.com/auth/login");
-        await page.getByTestId('email').fill("customer2@practicesoftwaretesting.com")
-        await page.getByTestId('password').fill("welcome01")
+        await page.getByTestId('email').fill("raeinten@gmail.com")
+        await page.getByTestId('password').fill("raeInTen*25")
         await page.getByTestId('login-submit').click();
+        await expect(page.getByTestId('nav-menu')).toContainText('Rae in');
     });
     test('search item',async({page})=>{
         await page.getByTestId('nav-home').click();
         await page.getByPlaceholder('search').fill("Pliers");
         await page.getByTestId('search-submit').click();
+        await expect(page.getByTestId('search_completed').getByRole("link")).toHaveCount(4);
     })
 });
